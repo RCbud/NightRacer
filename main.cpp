4643c5179b8935a85880bfe3f9382a45fbe45d6b
OBudiak <obudiak@kse.org.ua>
2025-10-31 22:51:33 +0200

    steering and shaking bug fixed

--- main.cpp
+++ main.cpp
@@ -23,9 +23,9 @@ struct Config {
     Color background = Color(50, 20, 30);
     RectangleShape playerShape = makeRect({160.f, 60.f}, {0.2f, 0.8f}, Color(80, 170, 220));
     Color playerColor = Color(220, 90, 90);
-    float angelSpeed = 150.f;
-    float moveSpeed = 400.f;
-    float acceleration = 250.f;
+    float angelSpeed = 520.f;
+    float moveSpeed = 1200.f;
+    float acceleration = 450.f;
     Keyboard::Key pauseKey = Keyboard::P;
     Keyboard::Key exitKey = Keyboard::Escape;
 };
@@ -45,8 +45,9 @@ class Game {
 public:
     Game(const Config& cfg)
     : cfg(cfg),
-      window(VideoMode(cfg.windowSize.x, cfg.windowSize.y), cfg.windowTitle, Style::Titlebar | Style::Close),
-      player(cfg.playerShape)
+      // window(VideoMode(cfg.windowSize.x, cfg.windowSize.y), cfg.windowTitle, Style::Titlebar | Style::Close),
+        window(sf::VideoMode::getDesktopMode(), cfg.windowTitle, sf::Style::Fullscreen),
+        player(cfg.playerShape)
     {
         window.setVerticalSyncEnabled(true);
         player.setFillColor(cfg.playerColor);
@@ -88,6 +89,11 @@ public:
 
 private:
     void processEvents() {
+        input.up    = Keyboard::isKeyPressed(Keyboard::W);
+        input.down  = Keyboard::isKeyPressed(Keyboard::S);
+        input.left  = Keyboard::isKeyPressed(Keyboard::A);
+        input.right = Keyboard::isKeyPressed(Keyboard::D);
+        input.space = Keyboard::isKeyPressed(Keyboard::Space);
         Event e;
         while (window.pollEvent(e)) {
             if (e.type == Event::Closed) {
@@ -106,12 +112,6 @@ private:
                 clock.restart();
             }
         }
-
-        input.up    = Keyboard::isKeyPressed(Keyboard::W);
-        input.down  = Keyboard::isKeyPressed(Keyboard::S);
-        input.left  = Keyboard::isKeyPressed(Keyboard::A);
-        input.right = Keyboard::isKeyPressed(Keyboard::D);
-        input.space = Keyboard::isKeyPressed(Keyboard::Space);
     }
 
     void update(const float * dt, float * speed, float * angelWheel, float * angel) {
@@ -120,26 +120,29 @@ private:
         float isAngel = 0.f;
         float isHandBrake = 0.f;
         if (input.up)    isAcceleration += 1.f;
-        if (input.down)  isAcceleration -= 1.f;
+        if (input.down)  isAcceleration -= 2.f;
         if (input.left)  isAngel -= 1.f;
         if (input.right) isAngel += 1.f;
         if (input.space) isHandBrake -= 1.f;
 
-        float dan = 180 * *dt;
+        float dan = 800 * *dt;
         *angelWheel += dan * isAngel;
         *angelWheel = *angelWheel > cfg.angelSpeed ? cfg.angelSpeed : *angelWheel < -cfg.angelSpeed ? -cfg.angelSpeed : *angelWheel; //limiting max angel speed
         float da = cfg.acceleration * *dt;
         *speed += da * isAcceleration;
         *speed = *speed > cfg.moveSpeed ? cfg.moveSpeed : *speed < -cfg.moveSpeed ? -cfg.moveSpeed : *speed; //limiting max speed
 
-        dir.x = cos((*angel + atan(30/110) * isAngel) * *dt);
-        dir.y = sin((*angel + atan(30/110) * isAngel) * *dt);
+        float angleRad = *angel * 3.14159265358979323846f / 180.f;
+        dir.x = cos(angleRad);
+        dir.y = sin(angleRad);
 
         Vector2f pos = player.getPosition();
         pos += dir * *speed * *dt;
 
         *angel += (*speed/cfg.moveSpeed * *angelWheel * *dt);
-        *angel -= *angel > 360.f ? 360.f : *angel < 0.f ? -360 : 0;
+        *angel = fmod(*angel, 360.f);
+        if (*angel < 0.f) *angel += 360.f;
+
 
         Vector2f ps =  cfg.playerShape.getSize();
         pos.x = max(ps.x, min(pos.x, (float)cfg.windowSize.x + ps.x));
